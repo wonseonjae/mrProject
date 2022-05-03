3becf3752130ef314647b6170c79600656b2772d
wonsunjae <bij7750@gmail.com>
2022-05-03 15:44:34 +0900

    2022-05-03 hadoop Tool setup, cleanup() 실습

---
+++ src/main/java/success/ResultCountMapper.java
@@ -0,0 +1,49 @@
+package success;
+
+import lombok.extern.log4j.Log4j;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+@Log4j
+public class ResultCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    String appName = "";
+    String resultCode = "";
+
+    @Override
+    protected void setup(Mapper<LongWritable, Text , Text, IntWritable>.Context context) throws IOException, InterruptedException {
+        Configuration conf = context.getConfiguration();
+
+        this.appName = conf.get("AppName");
+        this.resultCode = conf.get("resultCode");
+
+        log.info(this.appName+"난 map함수 실행전에 1번 실행되는 setup함수");
+    }
+
+    @Override
+    protected void cleanup(Mapper<LongWritable, Text, Text, IntWritable>.Context context) throws IOException, InterruptedException {
+        super.cleanup(context);
+        log.info(this.appName+"난 에러나도 무조건 실행되는 cleanup함수");
+    }
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+
+        String[] arr = line.split("\\W+");
+
+        int pos = arr.length-2;
+        String result = arr[pos];
+
+        log.info("[" + this.appName+"]" + result);
+
+        if (resultCode.equals(result)) {
+            context.write(new Text(result), new IntWritable(1));
+        }
+    }
+}
